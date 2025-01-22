329cdab2482efcca7b26f014890e2538d6ebda75
ggaans <ggaans@internode.on.net>
2025-01-22 14:11:06 +1030

    Added Storage Browser component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from '@aws-amplify/ui-react';
+import { StorageBrowser } from '../components/StorageBrowser';
 
 import outputs from "@/amplify_outputs.json";
 
@@ -38,8 +39,12 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+
+            {/* StorageBrowser Component */}
+            <h2>Your Files</h2>
+            <StorageBrowser />
         </main>
       )}
     </Authenticator>
