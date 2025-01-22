798deecbfc5727ea9e90b3d511d340a1ac1e63bf
ggaans <ggaans@internode.on.net>
2025-01-22 13:33:24 +1030

    Added Auth UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
+
 import outputs from "@/amplify_outputs.json";
+
+import "./../app/app.css";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
@@ -32,21 +35,13 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
